234dfc1b62d58555b665fe665ec4cd86be628861
Joshua Adkisson <112263154+Everardos@users.noreply.github.com>
2024-01-29 22:04:59 -0500

    Add function to play a single round

--- js/script.js
+++ js/script.js
@@ -10,9 +10,45 @@ function getComputerChoice() {
         case 3:
             choice = "scissors";
             break;
-        default:
-            choice = "shoot";
     }
     return choice;
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    let roundOutcome;
+    if (playerSelection === computerSelection) {
+        roundOutcome = "draw";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        roundOutcome = "lose";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        roundOutcome = "win";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        roundOutcome = "win";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        roundOutcome = "lose";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        roundOutcome = "lose";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        roundOutcome = "win";
+    }
+
+    switch (roundOutcome) {
+        case "win":
+            return `You win! ${playerSelection} beats ${computerSelection}!`;
+            break;
+        case "lose":
+            return `You lose! ${computerSelection} beats ${playerSelection}`;
+            break;
+        case "draw":
+            return `It's a tie! ${playerSelection} draws against ${computerSelection}`;
+            break;
+        case "explosion":
+            return `You messed up! Player: ${playerSelection}, computer: ${computerSelection}`;
+            break;
+    }
+}
+
+/*
+
+*/
